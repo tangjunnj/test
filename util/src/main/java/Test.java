249b8cc214c0bbad03bdd67f38602d07a21b6f85
JohnTang <john@JohndeMacBook-Air.local>
2016-10-24 21:58:34 +0800

    Initial commit

---
+++ util/src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by john on 16/10/22.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 5;
+        System.out.println(i+j);
+    }
+
+}
